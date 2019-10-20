a230016a7eeefc0e2c62b0308529f07b41e86ef4
Breno Morais <brenomorais88@gmail.com>
2019-10-20 12:50:46 -0300

    codigo inicial do projeto

---
+++ ConsultaRepositorios/ConsultaRepositorios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ConsultaRepositorios
+//
+//  Created by Kleyton Prestes Stringhetta on 20/10/19.
+//  Copyright © 2019 Breno Carvalho de Morais. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
